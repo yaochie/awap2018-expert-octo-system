3b5dc5a1cb794debbb65a6051af393c32587f865
ricsonc <ricsonc@andrew.cmu.edu>
2018-02-24 14:53:10 -0500

    move

--- src/baseline.py
+++ src/baseline.py
@@ -42,7 +42,6 @@ class Player(BasePlayer):
         self.list_graph = sorted(list(self.board.nodes(data=True)))
 
         self.frontier = self.get_frontier()
-        self.outer = self.get_outer()
         """
         Insert any player-specific turn initialization code here
         """
@@ -170,7 +169,7 @@ class Player(BasePlayer):
         """
 
         frontier_nodes = []
-        for node in self.outer:
+        for node in self.owned_frontier:
             # count number of neighbors that do not belong to us
             n_neighbors = 0
             for neighbor in self.board.neighbors(node):
@@ -199,7 +198,6 @@ class Player(BasePlayer):
         return {k: 0 for k in self.nodes}
 
     def execute_single_turn_actions(self):
-
         #move to frontier
         for nodes in self.nodes:
             if self.distance_from_frontier()[node] == 0:
@@ -214,7 +212,7 @@ class Player(BasePlayer):
             best_neighbor = min(neighbors, key = lambda x: self.distance_from_frontier()[x])
 
             self.verify_and_move_unit(nodes, best_neighbor, self_units-1)
-        
+
         #attacking
         for nodes in self.nodes:
             neighbors = self.board.neighbors(nodes)
